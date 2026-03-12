14e3fddbfadfed8f69f9e394ead74dcb489c35a6
sujithannabomina <sujith.boss01@gmail.com>
2026-03-13 00:59:38 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC9CSa4JSHpRHPpyKLB1eh9aOg5OeKWmCY",
+  authDomain: "delusu.firebaseapp.com",
+  projectId: "delusu",
+  storageBucket: "delusu.firebasestorage.app",
+  messagingSenderId: "1074810786294",
+  appId: "1:1074810786294:web:1e9f9f2e6bee043891b7d7"
 };
 
 const app = initializeApp(firebaseConfig);
